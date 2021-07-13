ec931337bb6f7bd135eec99dfeaea093648e8259
bisratawoke <bisratawokegithub>
2021-07-13 18:27:09 +0300

    ready for depl

--- pages/createProject.tsx
+++ pages/createProject.tsx
@@ -117,7 +117,7 @@ const createProject = ():ReactElement => {
 
                 setMssg('succesfully create project');
 
-                router.push('/');                
+                router.push('/project');                
 
                 return ;
             }
